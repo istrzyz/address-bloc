6466f31f90bc911e9c3fcd1dc00b0c766597b3a2
Ian Strzyzewski <istrzyz@gmail.com>
2015-09-29 08:53:38 -0700

    Assignment 21 Completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe AddressBook do
             book = AddressBook.new
             expect(book).to respond_to(:entries)
         end
+        
         it "should initialize entries as an array" do
             book = AddressBook.new
             expect(book.entries).to be_a(Array)
@@ -16,6 +17,25 @@ RSpec.describe AddressBook do
             expect(book.entries.size).to eq(0)
         end
     end
+    
+    describe "#remove_entry" do
+        it "removes an entry using the name, phone_number, and email" do
+            book = AddressBook.new
+            book.add_entry("Michael Faraday", "010.210.5181", "michael.faraday@faraday.com")
+            
+            name = "Ada Lovelace"
+            phone_number = "010.012.1815"
+            email_address = "augusta.king@lovelace.com"
+            book.add_entry(name, phone_number, email_address)
+            
+            
+            expect(book.entries.size).to eq 2
+            book.remove_entry(name, phone_number, email_address)
+            expect(book.entries.size) to eq 1
+            expect(book.entries.first.name).to eq("Michael Faraday")
+        end
+    end
+    
     
     describe "#add_entry" do
         it "adds only one entry to the address book" do
